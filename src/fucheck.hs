8dc3bd912b2497f3187ad44a30ffec4e7e2ecc2d
Sigurd Dam Sonniks <lsn777@alumni.ku.dk>
2020-02-18 14:54:38 +0100

    Looping tests

--- src/fucheck.hs
+++ src/fucheck.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
-import Data.Int
-import Foreign.Ptr
-import Foreign.Marshal.Alloc
-import Foreign.Marshal.Array
-import Foreign.Storable
---import System.Random
+import Data.Int (Int32)
+import Foreign.Ptr (Ptr)
+import Foreign.Marshal.Alloc (alloca)
+import Foreign.Storable (peek)
+import Data.List (unfoldr)
                        
+import System.Random (getStdGen, next, RandomGen)
 
 data Futhark_Context_Config
 foreign import ccall "futhark_context_config_new"
@@ -25,17 +25,39 @@ foreign import ccall "futhark_context_free"
 foreign import ccall "futhark_context_config_free"
   futhark_context_config_free :: Ptr Futhark_Context_Config -> IO ()
 
+next32 :: RandomGen g => g -> (Int32, g)
+next32 g = (toEnum int, newGen)
+  where (int,newGen) = next g
+
+myIterate :: (a -> (b,a)) -> a -> [b]
+myIterate f x = unfoldr (\x -> Just $ f x) x
+
+test :: Ptr Futhark_Context -> Int32 -> IO Bool
+test ctx seed =
+  alloca $ (\res -> do 
+    futhark_entry_test_entry ctx res seed
+    peek res)
+
+testLoop :: RandomGen g => Ptr Futhark_Context -> Int -> g -> IO ([Bool])
+testLoop ctx n gen = results
+  where
+    seeds = myIterate next32 gen
+    ios = map (test ctx) seeds
+    results = sequence $ take n ios
+  
 
 main :: IO ()
 main = do
   cfg <- futhark_context_config_new
   ctx <- futhark_context_new cfg
 
-  res <- alloca $ (\res -> do 
-    futhark_entry_test_entry ctx res 32
-    peek res)
+  gen <- getStdGen
+  
+  results <- testLoop ctx 100 gen 
 
-  putStrLn $ "Result " ++ show res
+  let result = all id results
+
+  putStrLn $ "Result " ++ show result
 
   futhark_context_free ctx
   futhark_context_config_free cfg
