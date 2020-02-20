3a3026b8565ec5a8964035801191ebc758148c83
Sigurd Dam Sonniks <lsn777@alumni.ku.dk>
2020-02-20 16:46:23 +0100

    Passing strings barely works

--- src/fucheck.hs
+++ src/fucheck.hs
@@ -1,11 +1,19 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
-import Data.Int (Int32)
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Codec.Binary.UTF8.String (decode)
+import Data.ByteString (pack)
+import Data.ByteString.UTF8 (toString)
+import qualified Data.ByteString.UTF8 as U
+import Data.Int  (Int32, Int64)
+import Data.Word (Word8)
 import Foreign.Ptr (Ptr)
 import Foreign.Marshal.Alloc (alloca)
+import Foreign.Marshal.Array (mallocArray, peekArray)
 import Foreign.Storable (peek)
 import Data.List (unfoldr)
-                       
 import System.Random (getStdGen, next, RandomGen)
+
 
 data Futhark_Context_Config
 foreign import ccall "futhark_context_config_new"
@@ -15,36 +23,67 @@ data Futhark_Context
 foreign import ccall "futhark_context_new"
   futNewContext :: Ptr Futhark_Context_Config -> IO (Ptr Futhark_Context)
 
-foreign import ccall "futhark_entry_runTest"
-  futEntry :: Ptr Futhark_Context -> Ptr Bool
-                           -> Int32 -> IO ()
-
 foreign import ccall "futhark_context_free"
   futFreeContext :: Ptr Futhark_Context -> IO ()
 
 foreign import ccall "futhark_context_config_free"
   futFreeConfig :: Ptr Futhark_Context_Config -> IO ()
 
-next32 :: RandomGen g => g -> (Int32, g)
-next32 g = (toEnum int, newGen)
-  where (int,newGen) = next g
 
-myIterate :: (a -> (b,a)) -> a -> [b]
-myIterate f x = unfoldr (\x -> Just $ f x) x
 
-test :: Ptr Futhark_Context -> Int32 -> IO Bool
-test ctx seed =
-  alloca $ (\res -> do 
-    futEntry ctx res seed
-    peek res)
 
-testLoop :: RandomGen g => Ptr Futhark_Context -> Int -> g -> IO ([Bool])
-testLoop ctx n gen = results
-  where
-    seeds = myIterate next32 gen
-    ios = map (test ctx) seeds
-    results = sequence $ take n ios
-  
+data Futhark_u8_1d
+
+-- New []u8
+foreign import ccall "futhark_new_i8_1d"
+  futNewArru8 :: Ptr Futhark_Context
+              -> Ptr Word8              -- The old array
+              -> Ptr Int64              -- The size
+              -> IO (Ptr Futhark_u8_1d) -- The fut array
+
+-- Move to C array
+foreign import ccall 
+  futhark_values_u8_1d :: Ptr Futhark_Context
+                       -> Ptr Futhark_u8_1d -- Old fut array
+                       -> Ptr Word8         -- New array
+                       -> IO Int32          -- Error info? Is this the right type?
+
+futValues :: Ptr Futhark_Context -> Ptr Futhark_u8_1d -> IO (Ptr Word8)
+futValues ctx futArr = do --(alloca :: (Ptr (Ptr Word8) -> (IO (Ptr Word8))) -> IO (Ptr Word8)) $ (\res -> do
+      shape    <- futShape ctx futArr
+      (cArrPtr :: Ptr (Ptr (Word8))) <- mallocArray shape
+      cArr     <- peek cArrPtr
+      futhark_values_u8_1d ctx futArr cArr
+      peek cArrPtr--)
+
+
+-- Get dimensions of fut array
+foreign import ccall 
+  futhark_shape_u8_1d :: Ptr Futhark_Context
+                      -> Ptr futhark_u8_1d       -- Array
+                      -> IO (Ptr Int)            -- size
+
+futShape :: Ptr Futhark_Context -> Ptr Futhark_u8_1d -> IO Int
+futShape ctx futArr = do
+  shapePtr <- futhark_shape_u8_1d ctx futArr
+  peek shapePtr
+
+-- Entry
+foreign import ccall 
+  futhark_entry_entrance :: Ptr Futhark_Context
+                         -> Ptr (Ptr Futhark_u8_1d) -- result
+                         -> Int32                   -- seed
+                         -> IO (Int32)              -- Possibly error msg?
+
+futEntry :: Ptr Futhark_Context -> Int32 -> IO (Ptr Futhark_u8_1d)
+futEntry ctx seed = futArr
+  where 
+    futArr =
+      alloca $ (\res -> do
+        futhark_entry_entrance ctx res seed
+        peek res)
+
+
 
 main :: IO ()
 main = do
@@ -52,12 +91,37 @@ main = do
   ctx <- futNewContext cfg
 
   gen <- getStdGen
-  
-  results <- testLoop ctx 100 gen 
+  --results <- testLoop ctx 100 gen 
+  --let result = all id results
 
-  let result = all id results
+  futResult <- futEntry ctx 87
+  cResult   <- futValues ctx futResult
+  shape     <- futShape ctx futResult
+  hsList    <- peekArray shape cResult
+  let str   =  toString $ pack hsList
+  putStrLn $ show shape
+  putStrLn $ str
+  --result <- futEntry ctx gen
 
-  putStrLn $ "Result " ++ show result
+  --first <- futValuesArru8 ctx result 0
+
+  --putStrLn $ show first
 
   futFreeContext ctx
   futFreeConfig cfg
+
+
+--testLoop :: RandomGen g => Ptr Futhark_Context -> Int -> g -> IO ([Bool])
+--testLoop ctx n gen = results
+--  where
+--    seeds = myIterate next32 gen
+--    ios = map (futEntry ctx) seeds
+--    results = sequence $ take n ios
+  
+next32 :: RandomGen g => g -> (Int32, g)
+next32 g = (toEnum int, newGen)
+  where (int,newGen) = next g
+
+myIterate :: (a -> (b,a)) -> a -> [b]
+myIterate f x = unfoldr (\x -> Just $ f x) x
+
--- src/futs/fucheck.fut
+++ src/futs/fucheck.fut
@@ -1,14 +1,21 @@
-import "test"
+import "lib/github.com/diku-dk/cpprandom/random"
+module dist = uniform_int_distribution i32 minstd_rand
+
 type result = #success | #failure i32
-type seed = i32
-
-let bool2result (seed : seed) (b : bool) : result =
-  match b
-  case true  -> #success
-  case false -> #failure seed
 
 
+let arbitrary (gen : minstd_rand.rng) : [100]i32 = 
+  let (_, is) = unzip <| map (dist.rand (1,6)) (minstd_rand.split_rng 100 gen)
+  in is
 
+let property  (is : [100]i32) : bool =
+  reduce (+) 0 (map (+1) is) == (reduce (+) 0 is + 100)
 
-entry runTest (seed : seed) : bool = Test.property (Test.gen seed)
+let runTest 't (arbitrary : minstd_rand.rng -> t) (property : t -> bool) (seed : i32) : bool =
+  let gen = minstd_rand.rng_from_seed [seed]
+  in property (arbitrary gen)
 
+ --let (rng, x) = dist.rand (1,6) rng
+entry entrance (seed : i32) : []u8 = "HejæøåÆØÅ med digÆ"
+
+--entry entrance (seed : i32) : bool = runTest arbitrary property seed
