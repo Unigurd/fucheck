d1d768469ae631cf488c9b878c3e8af16d470136
Sigurd Dam Sonniks <lsn777@alumni.ku.dk>
2020-04-06 20:32:08 +0200

    Loading fut functions dynamically

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+import qualified System.Posix.DynamicLinker as DL
 import Codec.Binary.UTF8.String (decode)
 import Data.ByteString (pack)
 import Data.ByteString.UTF8 (toString)
 import qualified Data.ByteString.UTF8 as U
 import Data.Int  (Int32, Int64)
 import Data.Word (Word8)
-import Foreign.Ptr (Ptr)
+import Foreign.Ptr (Ptr,FunPtr,castFunPtrToPtr,nullFunPtr)
 import Foreign.Marshal.Alloc (alloca)
 import Foreign.Marshal.Array (allocaArray, peekArray)
 import Foreign.Storable (Storable, peek)
@@ -15,48 +16,139 @@ import Data.List (unfoldr, foldl')
 import System.Random (randomIO, StdGen, getStdGen, next, RandomGen)
 import Control.Monad.Trans.Except(ExceptT(ExceptT),runExceptT)
 import Foreign.C.Types (CInt(CInt))
-
+import Control.Monad.IO.Class(liftIO)
+import Foreign.C.String (newCString)
 
 data Futhark_Context_Config
 foreign import ccall "futhark_context_config_new"
-  futNewConfig:: IO (Ptr Futhark_Context_Config)
+  oldFutNewConfig:: IO (Ptr Futhark_Context_Config)
+
+foreign import ccall "futhark_context_config_set_debugging"
+  oldSetDebug :: Ptr Futhark_Context_Config -> CInt -> IO ()
 
 data Futhark_Context
 foreign import ccall "futhark_context_new"
-  futNewContext :: Ptr Futhark_Context_Config -> IO (Ptr Futhark_Context)
+  oldFutNewContext :: Ptr Futhark_Context_Config -> IO (Ptr Futhark_Context)
 
 foreign import ccall "futhark_context_free"
-  futFreeContext :: Ptr Futhark_Context -> IO ()
+  oldFutFreeContext :: Ptr Futhark_Context -> IO ()
 
 foreign import ccall "futhark_context_config_free"
-  futFreeConfig :: Ptr Futhark_Context_Config -> IO ()
-
-right (Right r) = r
-
-
+  oldFutFreeConfig :: Ptr Futhark_Context_Config -> IO ()
 
 data Futhark_u8_1d
 data FutharkTestData
+type ValuesType =  Ptr Futhark_Context
+                -> Ptr Futhark_u8_1d -- Old fut array
+                -> Ptr Word8         -- New array
+                -> IO CInt          -- Error info? Is this the right type?
+
+type ArbitraryType =  Ptr Futhark_Context
+                   -> Ptr (Ptr FutharkTestData)
+                   -> CInt               -- size
+                   -> CInt               -- seed
+                   -> IO CInt
+
+type PropertyType =  Ptr Futhark_Context
+                  -> Ptr Bool
+                  -> Ptr FutharkTestData
+                  -> IO CInt
+
+type ShowType =  Ptr Futhark_Context
+              -> Ptr (Ptr Futhark_u8_1d)
+              -> Ptr FutharkTestData
+              -> IO CInt
+
+foreign import ccall "dynamic"
+  mkSetDebug :: FunPtr (Ptr Futhark_Context_Config -> CInt -> IO ())
+             ->         Ptr Futhark_Context_Config -> CInt -> IO () -- Svarer IO () til void?
+
+foreign import ccall "dynamic"
+  mkConfig :: FunPtr (IO (Ptr Futhark_Context_Config)) -> IO (Ptr Futhark_Context_Config)
+
+newFutConfig :: DL.DL -> IO (Ptr Futhark_Context_Config)
+newFutConfig dl = do
+  funCfg <- DL.dlsym dl "futhark_context_config_new"
+  cfg <- mkConfig funCfg
+  return cfg
+
+foreign import ccall "dynamic"
+  mkConfigFree :: FunPtr (Ptr Futhark_Context_Config -> IO ()) -> Ptr Futhark_Context_Config -> IO ()
+newFutFreeConfig :: DL.DL -> Ptr Futhark_Context_Config -> IO ()
+newFutFreeConfig dl cfg = do
+  f <- DL.dlsym dl "futhark_context_config_free"
+  mkConfigFree f cfg
+
+foreign import ccall "dynamic"
+  mkNewFutContext :: FunPtr (Ptr Futhark_Context_Config -> IO (Ptr Futhark_Context)) -> Ptr Futhark_Context_Config -> IO (Ptr Futhark_Context)
+newFutContext :: DL.DL -> Ptr Futhark_Context_Config -> IO (Ptr Futhark_Context)
+newFutContext dl cfg = do
+--  ctx_fun :: FunPtr (Ptr Futhark_Context_Config -> IO (Ptr Futhark_Context))
+  ctx_fun <- DL.dlsym dl "futhark_context_new" -- :: IO (FunPtr (Ptr Futhark_Context_Config -> IO (Ptr Futhark_Context))
+  mkNewFutContext ctx_fun cfg
+
+foreign import ccall "dynamic"
+  mkContextFree :: FunPtr (Ptr Futhark_Context -> IO ()) -> Ptr Futhark_Context -> IO ()
+freeFutContext dl ctx = do
+  f <- DL.dlsym dl "futhark_context_free"
+  mkContextFree f ctx
+
+foreign import ccall "dynamic"
+  mkFutValues :: FunPtr ValuesType -> ValuesType
+mkValues :: Ptr Futhark_Context -> FunPtr ValuesType -> Ptr Futhark_u8_1d -> ExceptT CInt IO String
+mkValues ctx valuesPtr futArr = ExceptT $ do
+  shape <- futShape ctx futArr
+  eitherArr <- runExceptT $ haskifyArr shape (mkFutValues valuesPtr ctx) futArr
+  case eitherArr of
+    Right hsList -> do
+      return $ Right $ decode hsList
+    Left errorcode -> return $ Left errorcode
+
+foreign import ccall "dynamic"
+  mkFutArb :: FunPtr ArbitraryType -> ArbitraryType
+
+mkArbitrary dl ctx name = do
+  arbPtr <- DL.dlsym dl ("futhark_entry_" ++ name)
+  return $ haskify2 (mkFutArb arbPtr) ctx
+
+foreign import ccall "dynamic"
+  mkFutProp :: FunPtr PropertyType -> PropertyType
+mkProperty dl ctx name = do
+  propPtr <- DL.dlsym dl ("futhark_entry_" ++ name)
+  return $ haskify (mkFutProp propPtr) ctx
+
+
+foreign import ccall "dynamic"
+  mkFutShow :: FunPtr ShowType -> ShowType
+mkShow dl ctx name = do
+  showPtr   <- DL.dlsym dl ("futhark_entry_" ++ name)
+  futValues <- DL.dlsym dl "futhark_values_u8_1d"
+  return $ \input -> do
+    u8arr <- haskify (mkFutShow showPtr) ctx input
+    mkValues ctx futValues u8arr
+
 
 haskify :: Storable out
-        => (Ptr out -> input -> IO CInt)
+        => (Ptr Futhark_Context -> Ptr out -> input -> IO CInt)
+        -> Ptr Futhark_Context
         -> input
         -> ExceptT CInt IO out
-haskify c_fun input =
+haskify c_fun ctx input =
   ExceptT $ alloca $ (\outPtr -> do
-    exitcode <- c_fun outPtr input
+    exitcode <- c_fun ctx outPtr input
     if exitcode == 0
     then (return . Right) =<< peek outPtr
     else return $ Left exitcode)
 
 haskify2 :: Storable out
-        => (Ptr out -> input1 -> input2 -> IO CInt)
+        => (Ptr Futhark_Context -> Ptr out -> input1 -> input2 -> IO CInt)
+        -> Ptr Futhark_Context
         -> input1
         -> input2
         -> ExceptT CInt IO out
-haskify2 c_fun input1 input2 =
+haskify2 c_fun ctx input1 input2 =
   ExceptT $ alloca $ (\outPtr -> do
-    exitcode <- c_fun outPtr input1 input2
+    exitcode <- c_fun ctx outPtr input1 input2
     if exitcode == 0
     then (return . Right) =<< peek outPtr
     else return $ Left exitcode)
@@ -105,39 +197,6 @@ futShape ctx futArr = do
   shapePtr <- futhark_shape_u8_1d ctx futArr
   peek shapePtr
 
--- Arbitrary
-foreign import ccall
-  futhark_entry_arbitrary :: Ptr Futhark_Context
-                          -> Ptr (Ptr FutharkTestData)
-                          -> CInt               -- size
-                          -> CInt               -- seed
-                          -> IO CInt
-
-futArbitrary :: Ptr Futhark_Context -> CInt -> CInt -> ExceptT CInt IO (Ptr FutharkTestData)
-futArbitrary ctx = haskify2 (futhark_entry_arbitrary ctx)
-
-
--- Property
-foreign import ccall
-  futhark_entry_property :: Ptr Futhark_Context
-                         -> Ptr Bool
-                         -> Ptr FutharkTestData
-                         -> IO CInt
-
-futProperty :: Ptr Futhark_Context -> Ptr FutharkTestData -> ExceptT CInt IO Bool
-futProperty ctx = haskify (futhark_entry_property ctx)
-
--- Show
-foreign import ccall
-  futhark_entry_show :: Ptr Futhark_Context
-                     -> Ptr (Ptr Futhark_u8_1d)
-                     -> Ptr FutharkTestData
-                     -> IO CInt
-
-futShow :: Ptr Futhark_Context -> Ptr FutharkTestData -> ExceptT CInt IO String
-futShow ctx input = do
-  u8arr <- haskify (futhark_entry_show ctx) input
-  futValues ctx u8arr
 
 
 spaces = ' ':spaces
@@ -184,9 +243,15 @@ data Result = Success
             | Failure (Either CInt String) CInt              -- input, seed
             | Exception (Maybe (Either CInt String)) Stage CInt CInt -- input, stage, error code, seed
 
+-- futArbitrary :: Ptr Futhark_Context -> CInt -> CInt -> ExceptT CInt IO (Ptr FutharkTestData)
+-- futProperty :: Ptr Futhark_Context -> Ptr FutharkTestData -> ExceptT CInt IO Bool
+-- futShow :: Ptr Futhark_Context -> Ptr FutharkTestData -> ExceptT CInt IO String
 data State = MkState
   {
     ctx             :: Ptr Futhark_Context
+  , arbitrary       :: CInt -> CInt -> ExceptT CInt IO (Ptr FutharkTestData)
+  , property        :: Ptr FutharkTestData -> ExceptT CInt IO Bool
+  , shower          :: Ptr FutharkTestData -> ExceptT CInt IO String
   , maxSuccessTests :: Int
   , computeSize     :: Int -> CInt
   , numSuccessTests :: Int
@@ -211,14 +276,14 @@ nextState state = (cInt, newState)
 someFun :: State -> IO Result
 someFun state = do
   let seed = getSeed state
-  eTestdata <- runExceptT $ futArbitrary (ctx state) (size state) seed
+  eTestdata <- runExceptT $ (arbitrary state) (size state) seed
   case eTestdata of
     Left arbExitCode -> return $ Exception Nothing Arb arbExitCode seed
     Right testdata -> do
-      eResult <- runExceptT $ futProperty (ctx state) testdata
+      eResult <- runExceptT $ (property state) testdata
       case eResult of
         Left propExitCode -> do
-          eStr <- runExceptT $ futShow (ctx state) testdata
+          eStr <- runExceptT $ (shower state) testdata
           case eStr of
             Right str -> return $ Exception (Just (Right str)) Test propExitCode seed
             Left showExitCode -> return $ Exception (Just (Left showExitCode)) Test propExitCode seed
@@ -226,7 +291,7 @@ someFun state = do
           if result
           then return Success
           else do
-            eStrInput <- runExceptT $ futShow (ctx state) testdata
+            eStrInput <- runExceptT $ (shower state) testdata
             return $ Failure eStrInput seed
 
 infResults :: State -> IO Result
@@ -262,16 +327,32 @@ result2str (Exception (Just (Left showExitCode)) stage exitCode seed) =
                               , ("show", [("Exit code", show showExitCode)])
                               ]
 
+
+right (Right a) = a
+
 main :: IO ()
 main = do
-  cfg <- futNewConfig
-  ctx <- futNewContext cfg
+  dl      <- DL.dlopen "src/futs/fucheck.so" [DL.RTLD_NOW]
+
+  cfg <- newFutConfig dl
+  ctx <- newFutContext dl cfg
+
+  dynArb  <- mkArbitrary dl ctx "arbitrary"
+  dynProp <- mkProperty  dl ctx "property"
+  dynShow <- mkShow dl ctx "show"
+
+
+
 
   gen <- getStdGen
   let state = MkState
         { ctx             = ctx
+        , arbitrary       = dynArb
+        , property        = dynProp
+        , shower          = dynShow
         , maxSuccessTests = 100
-        , computeSize     = toEnum . \n -> n `div` (maxSuccessTests state)
+        , computeSize     = toEnum . \n ->  n
+          -- (maxSuccessTests state) - (maxSuccessTests state) `div` (n+1)
         , numSuccessTests = 0
         , randomSeed      = gen
         }
@@ -279,5 +360,8 @@ main = do
   result <- infResults state
   putStrLn $ result2str result
 
-  futFreeContext ctx
-  futFreeConfig cfg
+  --oldFutFreeContext ctx
+  freeFutContext dl ctx
+  --oldFutFreeConfig cfg
+  newFutFreeConfig dl cfg
+  DL.dlclose dl
