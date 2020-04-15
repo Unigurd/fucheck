675226d04af1af38d1254d3fe6e582125737faa8
Sigurd Dam Sonniks <lsn777@alumni.ku.dk>
2020-04-15 12:44:14 +0200

    Revert "Handling showing failures\exceptions without show"
    This reverts commit 92de62985fdec5a20b7c2959afe34f113c8c10fc.

--- src/Main.hs
+++ src/Main.hs
@@ -344,8 +344,6 @@ nextState state = (cInt, newState)
     cInt         = toEnum int
     newState     = state {randomSeed = newGen}
 
-f *< a = f <*> pure a
-
 someFun :: State -> IO Result
 someFun state = do
   let seed = getSeed state
@@ -356,15 +354,28 @@ someFun state = do
       eResult <- runExceptT $ (property state) testdata
       case eResult of
         Left propExitCode ->
-          return $ Exception (stateTestName state) (shower state *< testdata) Test propExitCode seed
+          case shower state of
+            Nothing -> error "Jeg er for doven til at haandtere ikke-eksisterende shows"
+            Just showerdidum ->
+              case showerdidum testdata of
+                Right str ->
+                  return $ Exception (stateTestName state) (Just (Right str)) Test propExitCode seed
+                Left showExitCode ->
+                  return $ Exception (stateTestName state)
+                                     (Just (Left showExitCode))
+                                     Test
+                                     propExitCode seed
         Right result ->
           if result
           then return Success {resultTestName = stateTestName state, numTests = numSuccessTests state}
           else
-            return $ Failure { resultTestName = stateTestName state
-                             , shownInput     = shower state *< testdata
-                             , resultSeed     = seed
-                             }
+            case shower state of
+              Nothing -> error "for doven til Maybe show"
+              Just showshishow ->
+                return $ Failure { resultTestName = stateTestName state
+                                 , shownInput     = Just $ showshishow testdata
+                                 , resultSeed     = seed
+                                 }
 
 infResults :: State -> IO Result
 infResults state
